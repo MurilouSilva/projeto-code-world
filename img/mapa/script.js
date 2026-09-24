5e2ea11c12aa8f9c5b7cad3ab0ca2904e2897a1d
Isabelly França <isabelly.franca2000@gmail.com>
2026-09-24 11:47:34 -0300

    Arquivos organizados em pastas

---
+++ img/mapa/script.js
@@ -0,0 +1,3 @@
+// mapa/script.js
+// JavaScript da seção MAPA (territórios do CodeWorld).
+// Reservado para futuras interações desta tela (ex.: seleção de território).
---
+++ img/universo/script.js
@@ -0,0 +1,3 @@
+// universo/script.js
+// JavaScript da seção UNIVERSO (contexto, linguagens e linha do tempo).
+// Reservado para futuras interações desta tela.
